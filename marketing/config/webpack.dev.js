dd621f3f1bc6c1e910da0f5386647a6b4d032b84
utkarsh.kumar <utkarsh.kumar@lysto.io>
2022-06-24 15:04:47 +0530

    Marketing initial stable release

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const commonConfig = require("./webpack/common");
+const commonConfig = require("./webpack.common.js");
 
 const devConfig = {
   mode: "development",
